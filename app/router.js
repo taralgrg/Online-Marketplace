83a71205596b54b8592b987e198015071aaec306
Mac <taralgurung@hotmail.com>
2017-03-08 15:55:16 +0800

    added, tile and details

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('product', { path: '/product/:product_id/' });
 });
 
 export default Router;
