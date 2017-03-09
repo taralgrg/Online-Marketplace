2fbf5092f4010b5e1b2570e1cdd90240d6b9bb58
Mac <taralgurung@hotmail.com>
2017-03-09 17:37:51 +0800

    Display card added and functions

--- app/models/product.js
+++ app/models/product.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   model: DS.attr(),
   price: DS.attr(),
+  currency: DS.attr('string',{defaultValue:"HKD"}), 
   image: DS.attr()
 });
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  subTotal: 0,
   add(item) {
     this.get('items').pushObject(item);
+    // var total = this.get('subTotal');
+    this.set('subTotal', parseInt(item.get('price'))+parseInt(this.get('subTotal')));
   }
 });
